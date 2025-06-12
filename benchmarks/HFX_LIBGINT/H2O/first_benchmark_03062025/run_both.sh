d187cee6c3f5e639cd48864b1024515f56283cc3
marcelloPuligheddu <marcello.puligheddu@gmail.com>
2025-06-12 09:24:06 +0000

    added HIP benchmark results

---
+++ benchmarks/HFX_LIBGINT/H2O/first_benchmark_03062025/run_both.sh
@@ -0,0 +1,32 @@
+set -x
+module load fftw/3.3.10 fftw_mpi/3.3.10 gcc/base openmpi/5.0.7-ucc1.3.0-ucx1.18.0 hipfort/6.4.0
+
+export OMP_NUM_THREADS=1
+export HSA_XNACK=1
+
+mpirun -np 8 ../../../exe/local_hip/cp2k.psmp -i H2O-HFX-32.cpu.inp 2>&1 | tee 8_mpi_1_omp_libint
+wait
+mpirun -np 8 ../../../exe/local_hip/cp2k.psmp -i H2O-HFX-32.gpu.inp 2>&1 | tee 8_mpi_1_omp_libgint
+wait
+
+mpirun -np 4 ../../../exe/local_hip/cp2k.psmp -i H2O-HFX-32.cpu.inp 2>&1 | tee 4_mpi_1_omp_libint
+wait
+mpirun -np 4 ../../../exe/local_hip/cp2k.psmp -i H2O-HFX-32.gpu.inp 2>&1 | tee 4_mpi_1_omp_libgint
+wait
+
+mpirun -np 16 ../../../exe/local_hip/cp2k.psmp -i H2O-HFX-32.cpu.inp 2>&1 | tee 16_mpi_1_omp_libint
+wait
+mpirun -np 16 ../../../exe/local_hip/cp2k.psmp -i H2O-HFX-32.gpu.inp 2>&1 | tee 16_mpi_1_omp_libgint
+wait
+
+export OMP_NUM_THREADS=2
+mpirun -np 8 ../../../exe/local_hip/cp2k.psmp -i H2O-HFX-32.cpu.inp 2>&1 | tee 8_mpi_2_omp_libint
+wait
+export OMP_NUM_THREADS=4
+mpirun -np 8 ../../../exe/local_hip/cp2k.psmp -i H2O-HFX-32.cpu.inp 2>&1 | tee 8_mpi_4_omp_libint
+wait
+
+
+export OMP_NUM_THREADS=1
+set +x
+
