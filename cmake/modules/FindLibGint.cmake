eb9b718d23415b9e49d8f26d1104ba717bd23caa
mPuligheddu <marcello.puligheddu@gmail.com>
2026-05-27 15:59:22 +0000

    Fixed typos in find libs

--- cmake/modules/FindLibGint.cmake
+++ cmake/modules/FindLibGint.cmake
@@ -12,26 +12,26 @@
 include(FindPackageHandleStandardArgs)
 include(cp2k_utils)
 
-cp2k_find_libraries(LIBGINT ("Gint")
-cp2k_include_dirs(LIBGINT "libgint_wrapper.mod")
+cp2k_find_libraries(LIBGINT "cp2kGint")
+cp2k_include_dirs(LIBGINT "libgint.mod")
 
 if(CP2K_LIBGINT_INCLUDE_DIRS)
   find_package_handle_standard_args(
-    libgint DEFAULT_MSG CP2K_LIBGINT_LINK_LIBRARIES
+    lLibGint DEFAULT_MSG CP2K_LIBGINT_LINK_LIBRARIES
     CP2K_LIBGINT_INCLUDE_DIRS)
 else()
-  find_package_handle_standard_args(libgint DEFAULT_MSG
+  find_package_handle_standard_args(LibGint DEFAULT_MSG
                                     CP2K_LIBGINT_LINK_LIBRARIES)
 endif()
 
-if(NOT TARGET cp2k::libGint::libGint)
-  add_library(cp2k::libGint::libGint INTERFACE IMPORTED)
+if(NOT TARGET cp2k::LibGint::libGint)
+  add_library(cp2k::LibGint::libGint INTERFACE IMPORTED)
   set_target_properties(
-    cp2k::libGint::libGint PROPERTIES INTERFACE_LINK_LIBRARIES
-                                         "${CP2K_LIBSGINT_LINK_LIBRARIES}")
+    cp2k::LibGint::libGint PROPERTIES INTERFACE_LINK_LIBRARIES
+                                         "${CP2K_LIBGINT_LINK_LIBRARIES}")
   if(CP2K_LIBGINT_INCLUDE_DIRS)
     set_target_properties(
-      cp2k::libGint::libGint PROPERTIES INTERFACE_INCLUDE_DIRECTORIES
+      cp2k::LibGint::libGint PROPERTIES INTERFACE_INCLUDE_DIRECTORIES
                                            "${CP2K_LIBGINT_INCLUDE_DIRS}")
   endif()
 endif()
